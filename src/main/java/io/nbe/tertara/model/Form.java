6571a5e8c1e78f33943907026c86196e1655ad18
Nicolas Beaussart <nic.beaussart@gmail.com>
2018-11-09 14:21:25 +0100

    :construction: WIP ci

---
+++ src/main/java/io/nbe/tertara/model/Form.java
@@ -0,0 +1,42 @@
+package io.nbe.tertara.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import javax.persistence.*;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Table(name = "form")
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Form extends AuditModel {
+
+    @Id
+    @GeneratedValue(generator = "form_generator")
+    @SequenceGenerator(
+            name = "form_generator",
+            sequenceName = "form_sequence",
+            initialValue = 1000
+    )
+    private Long id;
+
+    @OneToMany(fetch = FetchType.LAZY)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Set<Question> questions;
+
+    @ColumnDefault("FALSE")
+    private boolean started;
+
+    @ColumnDefault("FALSE")
+    private boolean over;
+}
